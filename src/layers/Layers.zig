cbedeba2cadf0b9d99fa977a31f32631276cd91a
Lucas BEE <pouulet@gmail.com>
2025-10-19 23:35:17 +0200

    Small fixes

--- src/layers/Layers.zig
+++ src/layers/Layers.zig
@@ -37,7 +37,7 @@ pub fn fixSize(self: *Layers, total: u16) void {
 }
 
 pub fn addLayer(self: *Layers, layer: Layer) !void {
-    if (self.count + 1 >= max_layers) return error.TooManyLayers;
+    if (self.count >= max_layers) return error.TooManyLayers;
     self.entries[self.count] = layer;
     self.count += 1;
 }
@@ -46,4 +46,22 @@ pub fn format(self: *const Layers, writer: anytype) !void {
     for (self.entries[0..self.count]) |*layer| {
         try writer.print("{s: >6} : {f}\n", .{ layer.name(), layer });
     }
+}
+
+test "addLayer too many layers" {
+    const Range = @import("../range.zig").Range;
+
+    var layers = Layers{};
+    const udpLayer = Layer{ .udp = .{
+        .source = try Range(u16).init(1234, null),
+        .dest = try Range(u16).init(1234, null),
+    } };
+    for (0..max_layers) |_| {
+        try layers.addLayer(udpLayer);
+    }
+
+    // Check that we stopped with correctly at the max numbers of layers
+    try std.testing.expectEqual(max_layers, layers.count);
+    // Check that adding another layer returns an error
+    try std.testing.expectError(error.TooManyLayers, layers.addLayer(udpLayer));
 }
