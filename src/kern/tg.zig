327ee6def5e3d282a08e8970f9af864bfe4f0f10
Grégoire Delattre <gregoire.delattre@gmail.com>
2025-10-21 14:01:09 +0200

    Add a simple XDP program in the build

---
+++ src/kern/tg.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+const XdpAction = enum(c_long) {
+    XDP_ABORTED,
+    XDP_DROP,
+    XDP_PASS,
+    XDP_TX,
+    XDP_REDIRECT,
+};
+
+export const _license linksection("license") = "GPL".*;
+
+export fn tg_pass() linksection("xdp.frags") c_long {
+    return @intFromEnum(XdpAction.XDP_PASS);
+}
+
+export fn tg_drop() linksection("xdp.frags") c_long {
+    return @intFromEnum(XdpAction.XDP_DROP);
+}
