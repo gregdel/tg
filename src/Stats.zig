a8aa69e6c81d4d866a366701442ba4bb7ebfb989
Grégoire Delattre <gregoire.delattre@gmail.com>
2025-10-23 18:52:02 +0200

    Make the frame / packet distinction clearer

--- src/Stats.zig
+++ src/Stats.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 
 const Stats = @This();
 
-pending: u32 = 0,
-sent: u64 = 0,
+frames_pending: u32 = 0,
+frames_sent: u64 = 0,
 
 // AF_XDP socket stats
 rx_dropped: u64 = 0,
@@ -14,8 +14,8 @@ rx_fill_ring_empty_descs: u64 = 0,
 tx_ring_empty_descs: u64 = 0,
 
 pub fn add(self: *Stats, other: *const Stats) void {
-    self.pending += other.pending;
-    self.sent += other.sent;
+    self.frames_pending += other.frames_pending;
+    self.frames_sent += other.frames_sent;
     self.rx_dropped += other.rx_dropped;
     self.rx_invalid_descs += other.rx_invalid_descs;
     self.tx_invalid_descs += other.tx_invalid_descs;
@@ -26,8 +26,8 @@ pub fn add(self: *Stats, other: *const Stats) void {
 
 pub fn format(self: *const Stats, writer: anytype) !void {
     const fmt = "{s: >25}: {d}\n";
-    try writer.print(fmt, .{ "pending", self.pending });
-    try writer.print(fmt, .{ "frames_sent", self.sent });
+    try writer.print(fmt, .{ "frames pending", self.frames_pending });
+    try writer.print(fmt, .{ "frames sent", self.frames_sent });
     try writer.print(fmt, .{ "rx_dropped", self.rx_dropped });
     try writer.print(fmt, .{ "rx_invalid_descs", self.rx_invalid_descs });
     try writer.print(fmt, .{ "tx_invalid_descs", self.tx_invalid_descs });
