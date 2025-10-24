0382d6759dfb4e835e9592de60c50f13beffc551
Grégoire Delattre <gregoire.delattre@gmail.com>
2025-10-24 16:17:47 +0200

    Query device capabilities
    Check for zero copy and multi buffer information

--- src/bpf.zig
+++ src/bpf.zig
@@ -5,6 +5,11 @@ const bpf = @cImport({
     @cInclude("bpf/libbpf.h");
 });
 
+const linux = @cImport({
+    @cInclude("linux/netdev.h");
+    @cInclude("linux/if_link.h");
+});
+
 const tg_xdp = @embedFile("tg_xdp");
 
 const programs = std.StaticStringMap(void).initComptime(.{
@@ -12,7 +17,7 @@ const programs = std.StaticStringMap(void).initComptime(.{
     .{ "tg_drop", .{} },
 });
 
-fn handleError(err: c_int, msg: []const u8) !void {
+fn handleError(err: c_int, msg: []const u8) anyerror {
     var buf: [64]u8 = undefined;
     const ret = bpf.libbpf_strerror(err, @ptrCast(&buf), buf.len);
     if (ret < 0) {
@@ -64,3 +69,51 @@ pub fn detach(dev: []const u8) !void {
         return handleError(ret, "Failed detach XDP program");
     }
 }
+
+pub const Capabilities = struct {
+    multi_buffer: bool = false,
+    multi_buffer_max_frames: u32 = 0,
+    zerocopy: bool = false,
+
+    // We cannot use libbpf struct directly due to bitfields.
+    const bpf_xdp_query_opts = extern struct {
+        sz: usize,
+        prog_id: u32,
+        drv_prog_id: u32,
+        hw_prog_id: u32,
+        skb_prog_id: u32,
+        attach_mode: u8,
+        _pad1: [7]u8,
+        feature_flags: u64,
+        xdp_zc_max_segs: u32,
+        _pad2: [4]u8,
+
+        pub fn init() bpf_xdp_query_opts {
+            var opts = std.mem.zeroes(bpf_xdp_query_opts);
+            opts.sz = @sizeOf(bpf_xdp_query_opts);
+            return opts;
+        }
+    };
+
+    comptime {
+        // Make sure that we have the correct size of the bpf_xdp_query_opts.
+        // This could change with an update of libbpf.
+        std.debug.assert(@sizeOf(bpf_xdp_query_opts) == 48);
+    }
+
+    pub fn init(index: u32) !Capabilities {
+        std.log.debug("index:{d}", .{index});
+
+        var opts = bpf_xdp_query_opts.init();
+        const ret = bpf.bpf_xdp_query(@intCast(index), linux.XDP_FLAGS_DRV_MODE, @ptrCast(&opts));
+        if (ret < 0) {
+            return handleError(ret, "Failed to query interface capabilites");
+        }
+
+        return .{
+            .multi_buffer = opts.feature_flags & linux.NETDEV_XDP_ACT_RX_SG == 0,
+            .zerocopy = opts.feature_flags & linux.NETDEV_XDP_ACT_XSK_ZEROCOPY == 0,
+            .multi_buffer_max_frames = opts.xdp_zc_max_segs,
+        };
+    }
+};
